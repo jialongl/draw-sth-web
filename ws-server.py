f02b1285827b92d6d465b4c69d623bcbcf870f76
Liu Jialong <jialong08@gmail.com>
2012-04-08 04:41:52 +0800

    ws-server: simple communications between drawer and guesser

--- ws-server.py
+++ ws-server.py
@@ -4,49 +4,71 @@ import socket, threading, re
 import hashlib, base64
 import sys, getopt
 
+debug = True
+websocket_port = 8005
+
+def log (stuff):
+    global debug
+    if debug:
+        print stuff
+
+
 # The client sends a Sec-WebSocket-Key which is base64 encoded. To form a response, the magic string
 # "258EAFA5-E914-47DA-95CA-C5AB0DC85B11" is appended to this (undecoded) key, and the resulting string is then
 # hashed with SHA-1 and then base64 encoded. The result is then replied in the header "Sec-WebSocket-Accept".
 
-magic_string = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
-
-def handle_ws_request(soc):
-    global magic_string
-    request = soc.recv(1024)            # a buffer to hold the request
-#   print request
+def reply_ws_request (sock, addr):
+    magic_string = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
+    request = sock.recv(1024)                                         # a buffer to hold the request
+    log("New connection from " + str(addr))
+    log(request)
 
     match = re.search("(?<=Sec-WebSocket-Key: ).+", request)
-    key = match.group(0).strip()        # nasty "\r\n" at the end
+    key = match.group(0).strip()                                      # nasty "\r\n" at the end
     sha1 = hashlib.sha1()
     sha1.update(key + magic_string)
     h = base64.b64encode(sha1.digest())
 
     reply = "HTTP/1.1 101 Switching Protocols\r\nConnection: Upgrade\r\nUpgrade: websocket\r\nSec-WebSocket-Accept: %s\r\n\r\n" % h
-#   print reply
+    log(reply)
 
-    soc.send(reply)
-    stuff = True
-    while stuff:
-        stuff = soc.recv(4096)          # a larger buffer to hold whatever stuff coming
-        soc.send(stuff)                 # echo back
-    soc.close()
+    sock.send(reply)
+    log("Done handshaking.")
 
 
+def start_guessing_game (drawer_sock, guesser_sock):
+    le_answer = drawer_sock.recv(64)
 
-http_port = 8000                        # default to 8000, the same as "python -m SimpleHTTPServer"
-websocket_port = 8005
+    # send whatever stuff drawer_sock received to guesser_sock
+    data = True
+    while data:
+        data = drawer_sock.recv(4096)
+        guesser_sock.send(data)
 
-opts, args = getopt.getopt(sys.argv[1:], "o:l:")
-for o, v in opts:
-    if o == "-o":                       # ws origin
-        http_port = int(v)
-    elif o == "-l":                     # ws location
+
+opts, args = getopt.getopt(sys.argv[1:], "l:")
+for k, v in opts:
+    if k == "-l":                                                     # ws location
         websocket_port = int(v)
 
-soc = socket.socket()
-soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-soc.bind(('127.1', websocket_port))
-soc.listen(5)
-while 1:
-    t,_ = soc.accept()
-    threading.Thread(target = handle_ws_request, args = (t,)).start()
+listening_sock = socket.socket()
+listening_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+listening_sock.bind(('127.1', websocket_port))
+listening_sock.listen(2)
+
+queue_addrs = []
+queue_socks = []
+
+while True:
+    conn_sock, addr = listening_sock.accept()
+    reply_ws_request(conn_sock, addr)
+
+    queue_socks.append(conn_sock)
+    queue_addrs.append(addr)
+
+    if len(queue_addrs) >= 2:
+        sock1 = queue_socks.pop(0)
+        sock2 = queue_socks.pop(0)
+        addr1 = queue_addrs.pop(0)
+        addr2 = queue_addrs.pop(0)
+        threading.Thread(target = start_guessing_game, args = (sock1, sock2)).start()
